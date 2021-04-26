f7861b2d4bbc624a4828fdb70d2f2322a541f671
jaroslaw-wyrzutowicz-dragon <jaroslaw-wyrzutowicz-dragon@users.noreply.github.com>
2021-04-26 02:18:27 +0200

    commenting vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,21 +1,21 @@
 
-module "vpc" {
-  source  = "terraform-google-modules/network/google"
-  version = "0.6.0"
+# module "vpc" {
+#   source  = "terraform-google-modules/network/google"
+#   version = "0.6.0"
 
-  project_id   = "${var.project}"
-  network_name = "${var.env}"
+#   project_id   = "${var.project}"
+#   network_name = "${var.env}"
 
-  subnets = [
-    {
-      subnet_name   = "${var.env}-subnet-01"
-      subnet_ip     = "10.${var.env == "dev" ? 10 : 20}.10.0/24"
-      subnet_region = "us-west1"
-      subnet_flow_logs      = "false"
-    },
-  ]
+#   subnets = [
+#     {
+#       subnet_name   = "${var.env}-subnet-01"
+#       subnet_ip     = "10.${var.env == "dev" ? 10 : 20}.10.0/24"
+#       subnet_region = "us-west1"
+#       subnet_flow_logs      = "false"
+#     },
+#   ]
 
-  secondary_ranges = {
-    "${var.env}-subnet-01" = []
-  }
-}
+#   secondary_ranges = {
+#     "${var.env}-subnet-01" = []
+#   }
+# }
