2580fe1f0b9ae49dcb94db5da34c8c3e8a0fb8af
jaroslaw-wyrzutowicz-dragon <jaroslaw-wyrzutowicz-dragon@users.noreply.github.com>
2021-04-26 11:51:42 +0200

    commenting vpc

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -30,7 +30,7 @@ resource "google_compute_network" "vpc_network" {
 
 
 resource "google_datastore_index" "default" {
-  kind = "kind-test-2"
+  kind = "kind-test-3"
   properties {
     name = "property_a"
     direction = "ASCENDING"
