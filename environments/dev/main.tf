29bf3b56a879dca673d99a78edbf5e632a258821
jaroslaw-wyrzutowicz-dragon <jaroslaw-wyrzutowicz-dragon@users.noreply.github.com>
2021-04-27 13:52:16 +0200

    commenting vpc

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -29,7 +29,7 @@ resource "google_datastore_index" "default" {
   zone         = "us-central1-a"
   allow_stopping_for_update = true
 
-metadata_startup_script = "sudo apt-get update && gcloud auth configure-docker && sudo docker pull gcr.io/my-first-project-310908/docker-app-3:latest && sudo docker run -dp 3000:3000 docker-app-3"
+metadata_startup_script = "sudo apt-get update | gcloud auth configure-docker | sudo docker pull gcr.io/my-first-project-310908/docker-app-3:latest | sudo docker run -dp 3000:3000 docker-app-3"
 # apt-get update && sudo apt-get install apache2 -y && echo '<html><body><h1>Environment: ${local.network}</h1></body></html>' | sudo tee /var/www/html/index.html"
 
   boot_disk {
